1fb9ff52699985133ed6a4adfbe3f4b82ee5fa43
stefano <stefano.katsoras@gmail.com>
2022-06-30 22:40:38 -0400

    update logger format

--- cryptomart/globals.py
+++ cryptomart/globals.py
@@ -3,7 +3,7 @@ import logging
 
 from cryptomart.enums import Symbol
 
-LOGGING_FORMATTER = logging.Formatter("{levelname:<8} {name:<50}{funcName:>35}:{lineno:<4} {message}", style="{")
+LOGGING_FORMATTER = logging.Formatter("{asctime}.{msecs:03.0f} {levelname:<8} {name:<50}{funcName:>35}:{lineno:<4} {message}", style="{", datefmt="%Y-%m-%d %H:%M:%S")
 EARLIEST_OHLCV_DATE = datetime.datetime(2018, 1, 1)
 END_OHLCV_DATE = datetime.datetime(2022, 3, 11)
 INVALID_DATE = datetime.datetime(1980, 1, 1)
