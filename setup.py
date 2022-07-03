548b74a8d4d0d1678f24ab9e5e82a6ea090ee0de
stefano <stefano.katsoras@gmail.com>
2022-07-03 19:38:14 -0400

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="crypto-mart",
-    version="2.0.9",
+    version="2.0.10",
     author="Stefano Katsoras",
     author_email="stefano.katsoras@gmail.com",
     license="MIT",
